720dfce82dd668beb65bdaed999f0103e4571be0
Anna-Lara Buchner <67783991+anna-larabuchner@users.noreply.github.com>
2023-06-12 09:27:53 +0200

    Cleaning up, adding animation for section 4
    Adding scroll animation to section 4: content on left side slides out to left side, content on right side slides out to right side. New content zoomes in from middle.

--- script.js
+++ script.js
@@ -7,20 +7,7 @@ window.onload = function () {
   });
 
   // Add animation bindings to elements
-  lax.addElements(".selector", {
-    scrollY: {
-      opacity: [
-        ["elInY", "elCenterY", "elOutY"],
-        [0.3, 1, 1],
-      ],
-      scale: [
-        ["elInY", "elCenterY", "elOutY"],
-        [0.3, 1, 1],
-      ],
-    },
-  });
-
-  lax.addElements(".frame", {
+  lax.addElements(".videoloop__frame", {
     scrollY: {
       scale: [
         ["elInY", "elCenterY", "elOutY"],
@@ -40,5 +27,58 @@ window.onload = function () {
       ],
     },
   });
+
+  lax.addElements("#specialty__text-container", {
+    scrollY: {
+      opacity: [
+        ["elInY", "elCenterY", "elOutY"],
+        [1, 0.7, 0],
+      ],
+      translateX: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0, 0, -100],
+      ]
+    }
+  });
+
+  lax.addElements("#media-container__vid", {
+    scrollY: {
+      opacity: [
+        ["elInY", "elCenterY", "elOutY"],
+        [1, 0.7, 0],
+      ],
+      translateX: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0, 0, -100],
+      ]
+    }
+  });
+
+  lax.addElements("#media-container__img", {
+    scrollY: {
+      opacity: [
+        ["elInY", "elCenterY", "elOutY"],
+        [1, 0.7, 0],
+      ],
+      translateX: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0, 0, +100],
+      ]
+    }
+  });
+
+  lax.addElements(".break__frame", {
+    scrollY: {
+      opacity: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0, 0, 1],
+      ],
+      scale: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0, 0.3, 1],
+      ],
+    },
+  });
+
 };
 
