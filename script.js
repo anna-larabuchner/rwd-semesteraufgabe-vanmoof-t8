96f6b5276f010534be2868001a36a7d898f925f5
Anna-Lara Buchner <67783991+anna-larabuchner@users.noreply.github.com>
2023-06-12 08:15:51 +0200

    Finishing scroll animation for Section 2

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.onload = function () {
   lax.init();
-  console.log("init")
 
   // Add a driver that we use to control our animations
   lax.addDriver("scrollY", function () {
@@ -8,12 +7,38 @@ window.onload = function () {
   });
 
   // Add animation bindings to elements
-//   lax.addElements(".selector", {
-//     scrollY: {
-//       translateX: [
-//         ["elInY", "elCenterY", "elOutY"],
-//         [0, "screenWidth/2", "screenWidth"],
-//       ],
-//     },
-//   });
+  lax.addElements(".selector", {
+    scrollY: {
+      opacity: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0.3, 1, 1],
+      ],
+      scale: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0.3, 1, 1],
+      ],
+    },
+  });
+
+  lax.addElements(".frame", {
+    scrollY: {
+      scale: [
+        ["elInY", "elCenterY", "elOutY"],
+        {
+          599: [1, 1, 0.8],
+          600: [1, 1, 0.5],
+        }
+      ],
+    },
+  });
+
+  lax.addElements(".animation__fadeIn", {
+    scrollY: {
+      opacity: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0, 0, 1],
+      ],
+    },
+  });
 };
+
